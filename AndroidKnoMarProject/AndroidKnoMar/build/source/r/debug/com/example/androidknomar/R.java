0ec92fdbfd8e290a80d647266ffa31eaff815ffc
marchal <marchal@localhost>
2014-01-25 23:26:56 +0100

    loading sur la premi?re page

--- AndroidKnoMarProject/AndroidKnoMar/build/source/r/debug/com/example/androidknomar/R.java
+++ AndroidKnoMarProject/AndroidKnoMar/build/source/r/debug/com/example/androidknomar/R.java
@@ -24,14 +24,16 @@ public final class R {
         public static final int welcome=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
         public static final int container=0x7f080001;
         public static final int container_main=0x7f080000;
         public static final int imageView=0x7f080003;
-        public static final int menu_follow=0x7f080005;
-        public static final int menu_show_tweets=0x7f080007;
-        public static final int menu_show_user=0x7f080006;
+        public static final int menu_follow=0x7f080007;
+        public static final int menu_show_tweets=0x7f080009;
+        public static final int menu_show_user=0x7f080008;
+        public static final int progressBar=0x7f080004;
         public static final int textView=0x7f080002;
+        public static final int textView2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
