a062320b484c28b01f442f0ef2b2833f149bf499
mkr <mkr@localhost>
2014-01-25 15:34:19 +0100

    First Implementation of the corps

--- AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/util/AbstractParser.java
+++ AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/util/AbstractParser.java
@@ -3,15 +3,16 @@ package com.example.androidknomar.util;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-/**
- * Created by marchal.vincent on 25/01/14.
- */
 public class AbstractParser {
 
     private URI uri;
@@ -21,7 +22,7 @@ public class AbstractParser {
         uri = new URI(url);
     }
 
-    protected InputStream getStream() throws Exception {
+    public InputStream getStream() throws Exception {
         HttpClient c = new DefaultHttpClient();
         HttpGet r = new HttpGet(uri);
         try {
@@ -30,6 +31,23 @@ public class AbstractParser {
             e.printStackTrace();
             throw new Exception("Impossible to connect the http url :" + e.getMessage());
         }
+    }
+
+
+    public XmlPullParser initializer(InputStream inputStream) throws Exception
+    {
+        InputStreamReader reader = new InputStreamReader(inputStream);
+
+        XmlPullParserFactory factory;
+        XmlPullParser xpp;
+        try {
+            factory = XmlPullParserFactory.newInstance();
+            xpp = factory.newPullParser();
+            xpp.setInput(reader);
+        } catch (XmlPullParserException e) {
+            throw new Exception("Impossible to start the initalization : " + e.getMessage());
+        }
+        return xpp;
     }
 
     
