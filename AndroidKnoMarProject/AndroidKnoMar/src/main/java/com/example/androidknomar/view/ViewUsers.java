cff3ec6bffcd5c6114ea70321c53f57e942806be
Michel <Michel@localhost>
2014-01-25 23:21:57 +0100

    Modification de world

---
+++ AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/view/ViewUsers.java
@@ -0,0 +1,72 @@
+package com.example.androidknomar.view;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.androidknomar.R;
+import com.example.androidknomar.model.User;
+import com.example.androidknomar.model.World;
+
+/**
+ * Created by marchal.vincent on 25/01/14.
+ */
+public class ViewUsers extends ListActivity {
+
+    private World world;
+
+    public ViewUsers() {
+        super();
+        //setTitle("Follow someone");
+        world = World.instance;
+    }
+
+    protected Object mActionMode;
+    public int selectedItem = -1;
+
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        setContentView(R.layout.activity_users);
+
+        ArrayAdapter<User> array = new ArrayAdapter<User>(this,
+                android.R.layout.simple_list_item_multiple_choice,
+                world.getUsers());
+        setListAdapter(array);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        User user = (User) l.getItemAtPosition(position);
+        user.setIsFollowed(!user.isFollowed());
+        Log.e("Salut", user.getName() + " " + user.isFollowed());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.select_user_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.menu_follow:
+                // go to the next activity
+                Intent i = new Intent(this.getApplicationContext(), ViewTweets.class);
+                this.startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
