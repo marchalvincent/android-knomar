712afc04504ed054bf9002f008074cf82a016cb4
mkr <mkr@localhost>
2014-01-25 17:00:29 +0100

    Parsing User

---
+++ AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/model/User.java
@@ -0,0 +1,52 @@
+package com.example.androidknomar.model;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Created by marchal.vincent on 25/01/14.
+ */
+public class User implements Serializable
+{
+    public static final String CONST_USER = "user";
+    public static final String CONST_NAME = "name";
+
+    private String name;
+    private URI uri;
+    private boolean isFollowed = false;
+
+    private List<Tweet> listTweet;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public boolean isFollowed() {
+        return isFollowed;
+    }
+
+    public void setIsFollowed(boolean isFollowed) {
+        this.isFollowed = isFollowed;
+    }
+
+    public List<Tweet> getListTweet() {
+        return listTweet;
+    }
+
+    public void setListTweet(List<Tweet> listTweet) {
+        this.listTweet = listTweet;
+    }
+}
