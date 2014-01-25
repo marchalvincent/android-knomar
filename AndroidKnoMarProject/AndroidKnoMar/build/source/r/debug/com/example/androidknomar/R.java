f9038aa120b444eaac3cf7b70e02a46093deccf8
marchal <marchal@localhost>
2014-01-25 18:06:26 +0100

    tmp

--- AndroidKnoMarProject/AndroidKnoMar/build/source/r/debug/com/example/androidknomar/R.java
+++ AndroidKnoMarProject/AndroidKnoMar/build/source/r/debug/com/example/androidknomar/R.java
@@ -16,22 +16,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int welcome=0x7f020001;
+        public static final int no=0x7f020001;
+        public static final int ok=0x7f020002;
+        public static final int welcome=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
         public static final int container=0x7f080000;
+        public static final int icon=0x7f080004;
         public static final int imageView=0x7f080002;
+        public static final int label=0x7f080005;
         public static final int listView=0x7f080003;
+        public static final int menuitem1_show=0x7f080007;
         public static final int textView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int fragment_users=0x7f030002;
+        public static final int activity_users=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int fragment_users=0x7f030003;
+        public static final int rowlayout=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int rowselection=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
