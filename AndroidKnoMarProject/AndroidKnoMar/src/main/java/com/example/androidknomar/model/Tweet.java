45b1d9b66a7051ce724c6723e925cc1bd1c2fc1c
marchal <marchal@localhost>
2014-01-25 12:59:44 +0100

    debut parsing xml

--- AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/model/Tweet.java
+++ AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/model/Tweet.java
@@ -1,7 +1,17 @@
 package com.example.androidknomar.model;
 
+import java.net.URI;
+
 /**
  * Created by mknoertzer on 25/01/14.
  */
 public class Tweet {
+
+    private User user;
+    private String hashTag;
+    private String message;
+    private String date;
+    private URI url;
+
+
 }
---
+++ AndroidKnoMarProject/AndroidKnoMar/src/main/java/com/example/androidknomar/util/AbstractParser.java
@@ -0,0 +1,36 @@
+package com.example.androidknomar.util;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by marchal.vincent on 25/01/14.
+ */
+public class AbstractParser {
+
+    private URI uri;
+
+    public AbstractParser(String url) throws URISyntaxException {
+        super();
+        uri = new URI(url);
+    }
+
+    protected InputStream getStream() throws Exception {
+        HttpClient c = new DefaultHttpClient();
+        HttpGet r = new HttpGet(uri);
+        try {
+            return c.execute(r).getEntity().getContent();
+        } catch(IOException e) {
+            e.printStackTrace();
+            throw new Exception("Impossible to connect the http url :" + e.getMessage());
+        }
+    }
+
+    
+}
